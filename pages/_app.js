b499268a043b55cf9bb3582d18c98c94078d2718
ckybonist <ckybonist@gmail.com>
2020-12-22 23:14:58 +0800

    Add vconsole

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,20 @@
+import { useEffect } from 'react';
+import Head from 'next/head'
 import UserProvider from '../context/userContext'
 
 // Custom App to wrap it with context provider
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    import('vconsole').then(({ default: VConsole }) => {
+      const vConsole = new VConsole();
+      console.log('Hello v');
+    })
+  }, []);
+
   return (
     <UserProvider>
+      <Head>
+      </Head>
       <Component {...pageProps} />
     </UserProvider>
   )
