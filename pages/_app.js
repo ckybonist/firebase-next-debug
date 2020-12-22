50ff321afabae89c43b45ac7202bb72cfba2a9c0
ckybonist <ckybonist@gmail.com>
2020-12-22 23:07:05 +0800

    Revert "Add remote js"
    This reverts commit 50170264b7fad95dd87d5815799aaa7d5562a5b5.

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,9 @@
-import Head from 'next/head';
 import UserProvider from '../context/userContext'
 
 // Custom App to wrap it with context provider
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
-      <Head>
-        <script data-consolejs-channel="35ed2630-b859-b295-c7e6-f4c7a362d668" src="https://remotejs.com/agent/agent.js"></script>
-      </Head>
       <Component {...pageProps} />
     </UserProvider>
   )
